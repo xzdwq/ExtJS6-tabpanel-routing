ac4183df15b9fe3160dd9c2364c429081411766a
xzdwq <vazhivodrov@mail.ru>
2020-04-08 00:53:55 +0300

    ed

--- app/view/schedule/Schedule.js
+++ app/view/schedule/Schedule.js
@@ -1,3 +1,32 @@
+Ext.apply(Ext.form.VTypes, {
+  daterange: function(val, field) {
+    var date = field.parseDate(val);
+    if (!date) {
+      return false;
+    }
+    if (
+      field.startDateField &&
+      (!this.dateRangeMax || date.getTime() != this.dateRangeMax.getTime())
+    ) {
+      //var start = Ext.getCmp(field.startDateField);
+      var start = Ext.ComponentQuery.query('#start_date')[0];
+      start.setMaxValue(date);
+      start.validate();
+      this.dateRangeMax = date;
+    } else if (
+      field.endDateField &&
+      (!this.dateRangeMin || date.getTime() != this.dateRangeMin.getTime())
+    ) {
+      //var end = Ext.getCmp(field.endDateField);
+      var end = Ext.ComponentQuery.query('#end_date')[0];
+      end.setMinValue(date);
+      end.validate();
+      this.dateRangeMin = date;
+    }
+    return true;
+  }
+});
+
 Ext.define('lsk.view.schedule.Schedule', {
   extend: 'Ext.panel.Panel',
   alias: 'widget.schedule',
@@ -23,26 +52,259 @@ Ext.define('lsk.view.schedule.Schedule', {
         {
           xtype: 'panel',
           layout: { type: 'vbox' },
+          defaults: {
+            xtype: 'combobox',
+            width: 500,
+            labelWidth: 70,
+            labelAlign: 'right',
+            queryMode: 'local',
+            valueField: 'id',
+            displayField: 'name',
+            editable: false,
+            allowBlank: true
+          },
+          items: [
+            {
+              itemId: 'project_id',
+              fieldLabel: 'Проект',
+              emptyText: 'Выберите проект',
+              store: {
+                fields: [
+                  { name: 'id', type: 'int' },
+                  { name: 'code', type: 'string' },
+                  { name: 'name', type: 'string' }
+                ],
+                autoLoad: true,
+                proxy: {
+                  type: 'ajax',
+                  actionMethods: 'GET',
+                  api: { read: 'app/view/structur/structur.json' },
+                  startParam: '',
+                  limitParam: '',
+                  pageParam: '',
+                  pageSize: '',
+                  paramsAsJson: true,
+                  extraParams: { },
+                  reader: {
+                    type: 'json',
+                    rootProperty: 'result',
+                    successProperty: 'success',
+                    totalProperty: 'total'
+                  }
+                }
+              },
+              allowBlank: false,
+              displayField: 'name',
+              valueField: 'code',
+              listeners: {
+                afterrender: function() {
+                  this.getStore().on('load', function(store) {
+                    var prj = Ext.ComponentQuery.query('#project_id')[0];
+                    prj.select(prj.getStore().getAt(0));
+                  });
+                }
+              }
+            },
+            {
+              xtype: 'tagfield',
+              itemId: 'object_id',
+              fieldLabel: 'Объекты',
+              emptyText: 'Выберите объекты',
+              //store: { type: 'object' },
+              displayField: 'title'
+            }
+          ]
+        },
+        {
+          xtype: 'panel',
+          layout: { type: 'vbox' },
           items: [
             {
               xtype: 'panel',
               layout: { type: 'hbox' },
               defaults: {
-                width: 190,
+                width: 200,
                 labelWidth: 70,
                 labelAlign: 'right',
+                margin: '0 0 5 0',
+                editable: false
               },
               items: [
                 {
                   xtype: 'combobox',
-                  name: 'year',
+                  itemId: 'year',
                   fieldLabel: 'Год',
                   allowBlank: false,
-                  //store: { type: 'years' },
+                  store: {
+                    fields: ['year'],
+                    data: [
+                      { year: 2010 },
+                      { year: 2011 },
+                      { year: 2012 },
+                      { year: 2013 },
+                      { year: 2014 },
+                      { year: 2015 },
+                      { year: 2016 },
+                      { year: 2017 },
+                      { year: 2018 },
+                      { year: 2019 },
+                      { year: 2020 },
+                      { year: 2021 },
+                      { year: 2022 },
+                      { year: 2023 },
+                      { year: 2024 },
+                      { year: 2025 }
+                    ]
+                  },
+                  queryMode: 'local',
                   displayField: 'year',
-                  valueField: 'year'
+                  valueField: 'year',
+                  listeners: {
+                    afterrender: function() {
+                      var mnt = new Date();
+                      this.setValue(mnt.getFullYear());
+                    }
+                  }
+                },
+                {
+                  xtype: 'combobox',
+                  itemId: 'month',
+                  fieldLabel: 'Месяц',
+                  allowBlank: false,
+                  store: {
+                    fields: ['id', 'month'],
+                    data: [
+                      { id: 01, month: 'Январь' },
+                      { id: 02, month: 'Февраль' },
+                      { id: 03, month: 'Март' },
+                      { id: 04, month: 'Апрель' },
+                      { id: 05, month: 'Май' },
+                      { id: 06, month: 'Июнь' },
+                      { id: 07, month: 'Июль' },
+                      { id: 08, month: 'Август' },
+                      { id: 09, month: 'Сентябрь' },
+                      { id: 10, month: 'Октябрь' },
+                      { id: 11, month: 'Ноябрь' },
+                      { id: 12, month: 'Декабрь' }
+                    ]
+                  },
+                  queryMode: 'local',
+                  displayField: 'month',
+                  valueField: 'id',
+                  listeners: {
+                    afterrender: function() {
+                      var mnt = new Date();
+                      this.setValue(mnt.getMonth() + 1);
+                    }
+                  }
+                },
+                {
+                  xtype: 'radiofield',
+                  name: 'dateselector',
+                  inputValue: 'yearmon',
+                  checked: true,
+                  width: 20,
+                  margin: '0 0 0 5',
+                  listeners: {
+                    change: function (el, status) {
+                      var year = Ext.ComponentQuery.query('#year')[0];
+                      var month = Ext.ComponentQuery.query('#month')[0];
+                      if(status == false) {
+                        year.setDisabled(true);
+                        month.setDisabled(true);
+                      } else {
+                        year.setDisabled(false);
+                        month.setDisabled(false);
+                      }
+                    }
+                  }
                 }
               ]
+            },
+            {
+              xtype: 'panel',
+              layout: { type: 'hbox' },
+              defaults: {
+                width: 200,
+                labelWidth: 70,
+                labelAlign: 'right',
+                margin: '5 0 0 0'
+              },
+              items: [
+                {
+                  xtype: 'datefield',
+                  vtype: 'daterange',
+                  endDateField: 'end_date',
+                  format: 'd.m.Y',
+                  itemId: 'start_date',
+                  fieldLabel: 'Начало',
+                  allowBlank: false,
+                  disabled: true,
+                  listeners: {
+                    afterrender: function() {
+                      this.setValue(Ext.Date.getFirstDateOfMonth(new Date()))
+                    }
+                  }
+                },
+                {
+                  xtype: 'datefield',
+                  vtype: 'daterange',
+                  startDateField: 'start_date',
+                  format: 'd.m.Y',
+                  itemId: 'end_date',
+                  fieldLabel: 'Конец',
+                  allowBlank: false,
+                  disabled: true,
+                  listeners: {
+                    afterrender: function() {
+                      this.setValue(Ext.Date.getLastDateOfMonth(new Date()))
+                    }
+                  }
+                },
+                {
+                  xtype: 'radiofield',
+                  name: 'dateselector',
+                  inputValue: 'yearmon',
+                  checked: false,
+                  width: 20,
+                  margin: '5 0 0 5',
+                  listeners: {
+                    change: function (el, status) {
+                      var startdate = Ext.ComponentQuery.query('#start_date')[0];
+                      var enddate = Ext.ComponentQuery.query('#end_date')[0];
+                      if(status == true) {
+                        startdate.setDisabled(false);
+                        enddate.setDisabled(false);
+                      } else {
+                        startdate.setDisabled(true);
+                        enddate.setDisabled(true);
+                      }
+                    }
+                  }
+                }
+              ]
+            }
+          ]
+        },
+        {
+          xtype: 'panel',
+          layout: { type: 'vbox' },
+          defaults: {
+            width: 220,
+            margin: '0 0 5 15'
+          },
+          items: [
+            {
+              xtype: 'textfield',
+              itemId: 'task_code',
+              inputType: 'search',
+              emptyText: 'Введите код работы',
+              flex: 1,
+              allowBlank: true
+            },
+            {
+              xtype: 'button',
+              text: 'Применить'
             }
           ]
         }
